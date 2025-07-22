6b0211b5ca9df3d10e14a9875a22b913f7cf86d7
Abdulkarim Haddad <167714493+AKinThreads@users.noreply.github.com>
2025-07-21 20:38:24 -0400

    Created spotify album finder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,210 @@
+import "./App.css";
+import {
+  FormControl,
+  InputGroup,
+  Container,
+  Button,
+  Card,
+  Row,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-import './App.css'
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-  
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+  const [hoveredCardId, setHoveredCardId] = useState(null);
+
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    // Get Artist Albums
+    await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums?include_groups=album&market=US&limit=50",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setAlbums(data.items);
+      });
+  }
 
   return (
-   <>
-      <h1>Codédex Project Template</h1>
+    <>
+    <h1 style={{
+      textAlign: "center",
+      color: "white",
+      fontSize: "60px",
+      fontWeight: "bold",
+    }}>
+      Abdul's Spotify Album Search
+      </h1>
+      <Container>
+        <InputGroup>
+          <FormControl
+            placeholder="Search For Artist"
+            type="input"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            style={{
+              width: "300px",
+              height: "35px",
+              borderWidth: "0px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              marginRight: "10px",
+              paddingLeft: "10px",
+            }}
+          />
+          <Button 
+            onMouseEnter={() => setIsButtonHovered(true)}
+            onMouseLeave={() => setIsButtonHovered(false)}
+            onClick={search}
+            style={
+              isButtonHovered
+                ? {
+                    backgroundColor: "#66ff66",
+                    color: "#000000",
+                    boxShadow: "0 0 5px #66ff66",
+                    fontWeight: "bold",
+                    transition: "all 0.3s ease",
+                  }
+                : undefined
+            }
+          >
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+
+      <Container
+      style={{
+        marginTop: "50px",
+      }}
+      >
+        <Row
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "space-around",
+            alignContent: "center",
+            backgroundColor: "black",
+            
+          }}
+        >
+          {albums.map((album) => {
+            return (
+              <Card
+                key={album.id}
+                onMouseEnter={() => setHoveredCardId(album.id)}
+                onMouseLeave={() => setHoveredCardId(null)}
+                style={{
+                  backgroundColor: "#66ff66",
+                  margin: "10px",
+                  borderRadius: "5px",
+                  marginBottom: "30px",
+                  boxShadow: hoveredCardId === album.id ? "0 0 10px #66ff66" : "none",
+                  transition: "transform 1.0s ease",
+                }}
+              >
+                <Card.Img
+                  width={200}
+                  src={album.images[0].url}
+                  style={{
+                    borderRadius: "4%",
+                    border: "5px solid black",
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title
+                    style={{
+                      whiteSpace: "wrap",
+                      fontWeight: "bold",
+                      maxWidth: "200px",
+                      fontSize: "18px",
+                      marginTop: "10px",
+                      color: "black",
+                    }}
+                  >
+                    {album.name}
+                  </Card.Title>
+                  <Card.Text
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Release Date: <br /> {album.release_date}
+                  </Card.Text>
+                  <Button
+                    href={album.external_urls.spotify}
+                    style={{
+                      backgroundColor: "black",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "15px",
+                      borderRadius: "5px",
+                      padding: "10px",
+                    }}
+                  >
+                    Album Link
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
